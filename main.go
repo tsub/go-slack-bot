1002b27f5920e8e22d6402c24abb777a8e43aa8b
tsub <tsubasatakayama511@gmail.com>
2017-10-15 17:22:49 +0900

    Implement slack bot sample

--- main.go
+++ main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/lestrrat/go-slack"
+)
+
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	token := os.Getenv("SLACK_API_TOKEN")
+	cl := slack.New(token)
+
+	authers, err := cl.Auth().Test().Do(ctx)
+	if err != nil {
+		fmt.Printf("failed to test authentication: %s\n", err)
+		return
+	}
+	botID := authers.UserID
+
+	listener := NewSlackListener(cl, botID)
+
+	listener.ListenAndResponse()
 }
---
+++ slack.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/lestrrat/go-slack"
+	"github.com/lestrrat/go-slack/rtm"
+)
+
+type SlackListener struct {
+	Client *slack.Client
+	BotID string
+}
+
+func NewSlackListener(client *slack.Client, botID string) *SlackListener {
+	return &SlackListener{
+		Client: client,
+		BotID: botID,
+	}
+}
+
+func (s SlackListener) ListenAndResponse() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	rtmcl := rtm.New(s.Client)
+	go rtmcl.Run(ctx)
+
+	for e := range rtmcl.Events() {
+		switch typ := e.Type(); typ {
+		case rtm.MessageType:
+			if err := s.handleMessageEvent(ctx, e.Data().(*rtm.MessageEvent)); err != nil {
+				fmt.Printf("[ERROR] Failed to handle message: %s\n", err)
+			}
+		default:
+			// Noop
+		}
+	}
+}
+
+func (s SlackListener) handleMessageEvent(ctx context.Context, e *rtm.MessageEvent) error {
+	// Only mention
+	if !strings.Contains(e.Text, s.BotID) {
+		return nil
+	}
+
+	fmt.Printf("Mention event: %s\n", e)
+
+	if strings.Contains(e.Text, "hi") {
+		res, err := s.Client.Chat().PostMessage(e.Channel).Text("hi").Do(ctx)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Chat response: %s\n", res)
+	}
+
+	return nil
+}
